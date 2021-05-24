ad1d1d0ca16b9bb145b999d63638cc780644386d
Yiquan <yiquan2@illinois.edu>
2021-05-24 16:26:03 -0500

    change to mean fit

--- script/Plot_TrackPref.R
+++ script/Plot_TrackPref.R
@@ -12,16 +12,16 @@ library(gridExtra)
 require(cowplot)
 
 strain_levels <- c('HK68','Bk79','Bei89','Mos99','Vic11','HK19')
-YearPrefTable <- read_tsv('result/Prefs_ByYear.tsv') %>%
+YearPrefTable <- read_tsv('result/Fits_ByYear.tsv') %>%
                    mutate(Background=factor(Background,levels=strain_levels))
 
 palette  <- c(brewer.pal(9,"Set3"))
 palette  <- c(palette[3:12])
 textsize <- 7
-p <-  ggplot(YearPrefTable, aes(x=Year,y=MeanPref,group=Background,color=Background)) + 
+p <-  ggplot(YearPrefTable, aes(x=Year,y=Meanfit,group=Background,color=Background)) +
         geom_line(size=0.4) + 
         geom_point(size=0.4) +
-        geom_errorbar(aes(ymin=MeanPref-StdPref, ymax=MeanPref+StdPref), width=0) +
+        geom_errorbar(aes(ymin=Meanfit-Stdfit, ymax=Meanfit+Stdfit), width=0) +
         scale_color_manual(values=palette) + 
         theme_cowplot(12) +
         theme(plot.title=element_text(size=textsize,face="bold"),
@@ -30,5 +30,5 @@ p <-  ggplot(YearPrefTable, aes(x=Year,y=MeanPref,group=Background,color=Backgro
               legend.title=element_blank(),
               legend.text=element_text(size=textsize,face="bold"),
               legend.position='right') + 
-        ylab(bquote(bold(Mean~preference)))
-ggsave('graph/PrefsByYear.png',p,height=1.8,width=4.5)
+        ylab(bquote(bold(Mean~Fitness)))
+ggsave('graph/FitsByYear2.png',p,height=1.8,width=4.5)
