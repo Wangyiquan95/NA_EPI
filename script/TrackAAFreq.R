a29a14356b6f36b699bfb5e8e3cfbfdf26f98ed4
Yiquan <yiquan2@illinois.edu>
2021-05-23 22:00:48 -0500

    plot residues frequency by years

---
+++ script/TrackAAFreq.R
@@ -0,0 +1,50 @@
+#R code
+library(ggplot2)
+library(scales)
+library(RColorBrewer)
+library(readr)
+library(tidyr)
+library(reshape)
+library(stringr)
+library(dplyr)
+library(gridExtra)
+require(cowplot)
+
+plotaafreq <- function(aatable,poi,xlab,ylab){
+  postable    <- aatable %>%
+                   filter(grepl(poi,mut))
+  colorscale  <- c(brewer.pal(8,"Set2"))
+  palette     <- colorscale[1:length(unique(postable$mut))]
+  textsize    <- 9
+  p <- ggplot(postable,aes(year,freq,group=aa,color=aa)) +
+         geom_line() +
+         scale_color_manual(values=palette,drop=FALSE) +
+         theme_cowplot() +
+         theme(legend.title=element_blank(),
+               #legend.key.size=unit(3,"mm"),
+               legend.text=element_text(size=textsize,face="bold"),
+               axis.text=element_text(size=textsize,face="bold"),
+               axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),
+               axis.title=element_text(size=textsize,face="bold")) +
+         ylab(ylab) +
+         xlab(xlab) +
+         scale_y_continuous(breaks=c(0,0.5,1),labels=c('0%','50%','100%')) +
+         scale_x_continuous(breaks=c(1968,1980,1990,2000,2010,2020),labels=c(1968,1980,1990,2000,2010,2020)) +
+         guides(color=guide_legend(ncol=1))
+  return (p)
+  }
+
+aatable <- read_tsv('result/HumanN2Sweep_All.tsv') %>%
+             mutate(aa=mapply(function(s){return(str_sub(s,-1,-1))},mut)) %>%
+             mutate(mut=mapply(function(s){return(paste(str_sub(s,-1,-1),str_sub(s,2,-2),sep=''))},mut))
+p_328 <- plotaafreq(aatable,'328','','residue 328')
+p_329 <- plotaafreq(aatable,'329','','residue 329')
+p_344 <- plotaafreq(aatable,'344','','residue 344')
+p_367 <- plotaafreq(aatable,'367','','residue 367')
+p_368 <- plotaafreq(aatable,'368','','residue 368')
+p_369 <- plotaafreq(aatable,'369','','residue 369')
+p_370 <- plotaafreq(aatable,'370','','residue 370')
+
+#p <- grid.arrange(p_190,p_145,p_227,nrow=3)
+p <- grid.arrange(p_328,p_329,p_344,p_367,p_368,p_369,p_370,nrow=7,ncol=1)
+ggsave('graph/NatMutFreq_roi.png',p,height=8.5,width=5)
