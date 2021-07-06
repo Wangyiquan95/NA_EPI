0a74355f7ab34621b7940ec005bebe6acc22960f
Yiquan <yiquan2@illinois.edu>
2021-07-06 14:08:50 -0500

    Plot correlation of HA epistasis across different strains

---
+++ script/Plot_CorEPI_HA.R
@@ -0,0 +1,110 @@
+# Title     : correlate EPI among diferent strains on HA
+# Objective :
+# Created by: yiquan
+# Created on: 6/7/21
+
+library(ggplot2)
+library(scales)
+library(RColorBrewer)
+library(readr)
+library(tidyr)
+library(reshape)
+library(stringr)
+library(dplyr)
+library(ggrepel)
+library(gridExtra)
+library(data.table)
+library(GGally)
+library(e1071)
+library(sinaplot)
+library(ggforce)
+require(cowplot)
+
+
+
+multmerge <- function(mypath,key){
+  df <- list.files(path =mypath,pattern = key,full.names = T) %>%
+    lapply(read_csv) %>% bind_rows
+  return(df)
+}
+###NA model EPI
+# HK68_epi <- read.csv('result/HK68_epi.csv') %>% mutate(HK68=EPI)
+# Bk79_epi <- read.csv('result/Bk79_epi.csv')%>% mutate(bK79=EPI)
+# Bei89_epi <- read.csv('result/Bei89_epi.csv')%>% mutate(Bei89=EPI)
+# Mos99_epi <- read.csv('result/Mos99_epi.csv')%>% mutate(Mos99=EPI)
+# Vic11_epi <- read.csv('result/Vic11_epi.csv')%>% mutate(Vic11=EPI)
+# HK19_epi <- read.csv('result/HK19_epi.csv')%>% mutate(HK19=EPI)
+###HA model EPI
+HK68_epi <- read.csv('result/HA_HK68_epi.csv') %>% mutate(HK68=EPI)
+Bk79_epi <- read.csv('result/HA_Bk79_epi.csv')%>% mutate(bK79=EPI)
+Bei89_epi <- read.csv('result/HA_Bei89_epi.csv')%>% mutate(Bei89=EPI)
+Mos99_epi <- read.csv('result/HA_Mos99_epi.csv')%>% mutate(Mos99=EPI)
+Bris07_epi <- read.csv('result/HA_Bris07_epi.csv')%>% mutate(Bris07=EPI)
+ND16_epi <- read.csv('result/HA_NDako16_epi.csv')%>% mutate(ND16=EPI)
+#merge df
+com_epi_df <- bind_cols(list(HK68_epi,Bk79_epi,Bei89_epi,Mos99_epi,Bris07_epi,ND16_epi)) %>%
+  select(2:5,7,14,21,28,35,42)
+textsize <- 7
+
+
+p  <- ggpairs(com_epi_df,columns=5:10,lower =list(continuous=wrap(ggally_points,size=0.1,alpha=1)),
+		upper = list(continuous="blank"),
+		diag  = list(continuous ="blank")) +
+  theme_cowplot(9) +
+  theme(plot.title=element_text(size=textsize,face="bold"),
+            panel.grid.major = element_blank(),
+            legend.text=element_text(size=textsize,face="bold"),
+            axis.title=element_text(size=textsize,face="bold"),
+            axis.text=element_text(size=textsize,face="bold"))+
+  theme(plot.title=element_text(size=textsize,face="bold"),
+            panel.grid.major = element_blank(),
+            legend.text=element_text(size=textsize,face="bold"),
+            axis.title=element_text(size=textsize,face="bold"),
+            axis.text=element_text(size=textsize,face="bold")) +
+  scale_x_continuous(n.breaks = 3.5) +
+  scale_y_continuous(n.breaks = 4)
+ggsave('graph/HA_compare_EPI.png',p,width = 4, height = 4)
+
+print (cor(data.table(select(com_epi_df, 5:10))))
+
+#####     compare additive correlation      ##########
+
+###Load NA model add
+# HK68_add <- read.csv('result/HK68_add.csv') %>% mutate(HK68=add_fit)
+# Bk79_add <- read.csv('result/Bk79_add.csv')%>% mutate(bK79=add_fit)
+# Bei89_add <- read.csv('result/Bei89_add.csv')%>% mutate(Bei89=add_fit)
+# Mos99_add <- read.csv('result/Mos99_add.csv')%>% mutate(Mos99=add_fit)
+# Vic11_add <- read.csv('result/Vic11_add.csv')%>% mutate(Vic11=add_fit)
+# HK19_add <- read.csv('result/HK19_add.csv')%>% mutate(HK19=add_fit)
+###Load HA model add
+HK68_add <- read.csv('result/HA_HK68_add.csv') %>% mutate(HK68=add_fit)
+Bk79_add <- read.csv('result/HA_Bk79_add.csv')%>% mutate(Bk79=add_fit)
+Bei89_add <- read.csv('result/HA_Bei89_add.csv')%>% mutate(Bei89=add_fit)
+Mos99_add <- read.csv('result/HA_Mos99_add.csv')%>% mutate(Mos99=add_fit)
+Bris07_add <- read.csv('result/HA_bRIS07_add.csv')%>% mutate(Bris07=add_fit)
+ND16_add <- read.csv('result/HA_ndAKO16_add.csv')%>% mutate(ND16=add_fit)
+#merge df
+com_add_df <- bind_cols(list(HK68_add,Bk79_add,Bei89_add,Mos99_add,Bris07_add,ND16_add)) %>%
+  select(2:3,5,10,15,20,25,30)
+textsize <- 7
+
+
+p2  <- ggpairs(com_add_df,columns=3:8,lower =list(continuous=wrap(ggally_points,size=0.1,alpha=1)),
+		upper = list(continuous="blank"),
+		diag  = list(continuous ="blank")) +
+  theme_cowplot(9) +
+  theme(plot.title=element_text(size=textsize,face="bold"),
+            panel.grid.major = element_blank(),
+            legend.text=element_text(size=textsize,face="bold"),
+            axis.title=element_text(size=textsize,face="bold"),
+            axis.text=element_text(size=textsize,face="bold"))+
+  theme(plot.title=element_text(size=textsize,face="bold"),
+            panel.grid.major = element_blank(),
+            legend.text=element_text(size=textsize,face="bold"),
+            axis.title=element_text(size=textsize,face="bold"),
+            axis.text=element_text(size=textsize,face="bold")) +
+  scale_x_continuous(n.breaks = 3.5) +
+  scale_y_continuous(n.breaks = 4)
+ggsave('graph/HA_compare_ADD.png',p2,width = 4, height = 4)
+
+print (cor(data.table(select(com_add_df, 3:8))))
